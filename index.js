54b2267163ede462c379e56b11979d9dfdd3d3d7
Akash-S-G <akashgoundi100@gmail.com>
2024-09-28 11:33:46 +0530

    Added Background Music.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 setInterval(() => {
     let music=new Audio('sound.mp3');
 music.play()
-}, 2000);
+music.volume=0.1;
+
+}, 1000);
+let musicp=new Audio('game_.mp3')
+musicp.play();
+// musicp.volume=0.5;
