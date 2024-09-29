c403689b7fe3510a0ea781da41152472b40560e5
Akash-S-G <akashgoundi100@gmail.com>
2024-09-29 12:31:08 +0530

    Added Trees And Updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ setInterval(() => {
     let music=new Audio('sound.mp3');
 music.play()
 music.volume=0.1;
-
 }, 1000);
-let musicp=new Audio('Music.mp3')
+
+document.addEventListener("click",()=>{
+    let musicp=new Audio('Music.mp3')
+musicp.loop=true;
 musicp.play();
+})
